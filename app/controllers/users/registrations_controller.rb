b5802a13987e0b63c05050b1ad2b42d52172e1ff
Rudi <56592445+rjfz@users.noreply.github.com>
2021-09-21 20:38:50 +0100

    added page formatting on login/edit/signup (#13)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,5 +4,20 @@ module Users
     def configure_account_update_params
       devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
     end
+    def after_sign_up_path_for(resource)
+      homepage_url
+    end
+    def after_sign_in_path_for(resource)
+      homepage_url
+    end
+    def after_sign_out_path_for(resource)
+      homepage_url
+    end
+
+    # protected
+
+    # def update_resource(resource, params)
+    #   resource.update_without_password(params)
+    # end
   end
 end
