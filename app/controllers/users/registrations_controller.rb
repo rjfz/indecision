9cfdb8ad87767ab2b3fd0363985d7e4a8de130dc
Rudi <56592445+rjfz@users.noreply.github.com>
2021-09-23 17:46:21 +0100

    added dashboard for logged in users (#16)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,10 +5,10 @@ module Users
       devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
     end
     def after_sign_up_path_for(resource)
-      homepage_url
+      dashboard_url
     end
     def after_sign_in_path_for(resource)
-      homepage_url
+      dashboard_url
     end
     def after_sign_out_path_for(resource)
       homepage_url
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'home#homepage', as: :homepage
+  get '/dashboard', to: 'dashboard#dashboard', as: :dashboard
 end
