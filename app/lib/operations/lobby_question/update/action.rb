a5c1e380f1be1ed4f0721c5dcdb0df2539221c72
rjfz <rjsfeeney@gmail.com>
2021-11-28 19:02:57 +0000

    fixing states for lobbies, cant rerun questions anymore etc.

--- app/lib/operations/lobby_question/update/action.rb
+++ app/lib/operations/lobby_question/update/action.rb
@@ -4,17 +4,22 @@ module Operations
   module LobbyQuestion
     module Update
       class Action
-        def initialize(lobby_question, form)
+        include LobbyDependencies['update_lobby']
+
+        def initialize(lobby_question, form, **deps)
+          super(**deps)
+
           @form = form
           @lobby_question = lobby_question
         end
 
         def call
+          raise IncorrectStateError if lobby_question.state == 'finished'
+
           lobby_question.assign_attributes attributes
           Command.save(lobby_question).tap do |lobby_question|
-            if lobby_question.starting?
-              StartLobbyQuestionJob.set(wait_until: lobby_question.started_at).perform_later(lobby_question)
-            end
+            starting(lobby_question)
+            finished(lobby_question)
           end
         end
 
@@ -26,6 +31,19 @@ module Operations
           form.attributes.tap do |attrs|
             attrs[:started_at] = (Time.now + 5.seconds) if attrs[:state] == 'starting'
           end
+        end
+
+        def starting(lobby_question)
+          return unless lobby_question.starting?
+
+          StartLobbyQuestionJob.set(wait_until: lobby_question.started_at).perform_later(lobby_question)
+          update_lobby.call(lobby: lobby_question.lobby, state: 'started')
+        end
+
+        def finished(lobby_question)
+          return unless lobby_question.finished? && lobby_question.lobby.lobby_questions.pluck(:state).all?('finished')
+
+          update_lobby.call(lobby: lobby_question.lobby, state: 'finished')
         end
       end
     end
