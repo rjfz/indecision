ebcc73f2e32a39f7b46e297ca1bc3adbd5debfc7
rjfz <rjsfeeney@gmail.com>
2022-03-06 23:15:00 +0000

    add tests to operations

--- spec/operations/anon_user_lobby/create/entry_point_spec.rb
+++ spec/operations/anon_user_lobby/create/entry_point_spec.rb
@@ -5,8 +5,15 @@ require 'rails_helper'
 RSpec.describe Operations::AnonUserLobby::Create::EntryPoint do
   subject { described_class.call(params) }
 
-  let(:anon_user_lobby) { create(:anon_user_lobby) }
+  let(:params) do
+    {
+      lobby_id: lobby.id,
+      anon_user_id: anon_user.id
+    }
+  end
 
+  let(:lobby){create(:lobby)}
+  let(:anon_user){create(:anon_user)}
 
   context 'when all parameters are supplied' do
     it_behaves_like 'create entrypoint', AnonUserLobby
